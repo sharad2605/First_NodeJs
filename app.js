02a88321af4e3c0a2a8b50a70c31d88a7704aac6
sharad2605 <sharad26shukla@gmail.com>
2026-07-16 12:34:52 +0530

    Method 1Export the function directly

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
-const http= require('http');
-const server= http.createServer((req, res) => { 
-        console.log("server is created"); 
-        
-        res.setHeader('Content-Type', 'text/html');
-         if (req.url === "/home") {
-        res.end("<h1>Welcome home</h1>");
-    } 
-    else if (req.url === "/about") {
-        res.end("<h1>Welcome to About Us</h1>");
-    } 
-    else if (req.url === "/node") {
-        res.end("<h1>Welcome to my Node Js project</h1>");
-    } 
-    else {
-        res.end("<h1>Page Not Found</h1>");
-    }
-    });
+const http = require("http");
 
+const requestHandler = require("./routes");
 
-let port =3000    
-server.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+const server = http.createServer(requestHandler);
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
 });
-
